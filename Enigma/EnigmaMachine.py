fe271d6fabbe517c30fb33640466cbae59d1692b
Jeremy Turinetti <jeremy.turinetti@gmail.com>
2015-03-23 12:14:08 -0500

    comments and long input test

--- Enigma/EnigmaMachine.py
+++ Enigma/EnigmaMachine.py
@@ -1,3 +1,17 @@
+# Thanks to the following pages for the information and education I needed.
+# http://red-badger.com/blog/2015/02/23/understanding-the-enigma-machine-with-30-lines-of-ruby-star-of-the-2014-film-the-imitation-game/
+# http://en.wikipedia.org/wiki/Enigma_machine
+# http://en.wikipedia.org/wiki/Enigma_rotor_details
+
+# Version 1 - 3/23/2015
+# Basic Enigma functionality
+#   * Random plugboard configuration generated for each newly created EnigmaMachine object; not configurable at this time
+#   * Only supports 3 rotors
+#   * Does not support double-stepping of second rotor
+#   * Limited configuration options for rotors and reflector
+#   * Only supports English alphabet
+#   * All messages are converted to upper-case
+
 from Plugboard import Plugboard
 from Rotors import Rotors
 from Reflector import Reflector
@@ -27,5 +41,7 @@ class EnigmaMachine:
         return output
     
     def ResetMachine(self):
+        print()
         print('Resetting machine...')
+        print()
         self.rotors.ResetRotors()
--- Enigma/main.py
+++ Enigma/main.py
@@ -2,18 +2,22 @@ from EnigmaMachine import EnigmaMachine
 
 def main():
     e = EnigmaMachine()
-    output = e.ProcessMessage('HELLOWORLD')
+    output = e.ProcessMessage('HELLOWORLD')    
     e.ResetMachine()
     output2 = e.ProcessMessage(output)
-
-    print()
-    e.ResetMachine()
-    print()
+    
+    e.ResetMachine()    
 
     output = e.ProcessMessage('GGGGGGGGGGGGGGGGGGGGGGGGGGGGGG')
     e.ResetMachine()
-    output2 = e.ProcessMessage(output)   
+    output2 = e.ProcessMessage(output)    
     
+    e.ResetMachine()    
+    
+    long_output = e.ProcessMessage('GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG')   
+    e.ResetMachine()
+    long_output2 = e.ProcessMessage(long_output)
+
     print()
     print('Done...')
     print() 
