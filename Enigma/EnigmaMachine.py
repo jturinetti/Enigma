33333132ed47d81335f1a49f13bff0cb673befb6
jturinetti <jeremy.turinetti@gmail.com>
2015-03-24 09:55:03 -0500

    removing dumb header comment

--- Enigma/EnigmaMachine.py
+++ Enigma/EnigmaMachine.py
@@ -1,5 +1,4 @@
 # Enigma Machine Simulator
-# Python v
 
 # Thanks to the following pages for the information and education I needed.
 # http://red-badger.com/blog/2015/02/23/understanding-the-enigma-machine-with-30-lines-of-ruby-star-of-the-2014-film-the-imitation-game/
