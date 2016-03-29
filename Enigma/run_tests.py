2b401573edb6ce3b8087e81690a8ca1cf908a6ac
Jeremy <jeremy.turinetti@gmail.com>
2016-03-28 20:39:38 -0500

    added launch.json file for VS Code support; added test harness

---
+++ Enigma/run_tests.py
@@ -0,0 +1,17 @@
+import unittest
+from UnitTests import full_messages
+from UnitTests import invalid_constructor_args
+from UnitTests import invalid_method_args
+from UnitTests import test_messages
+
+if __name__ == '__main__':
+    loader = unittest.TestLoader()
+    suite = unittest.TestSuite((
+        loader.loadTestsFromTestCase(full_messages.full_messages),
+        loader.loadTestsFromTestCase(invalid_constructor_args.invalid_constructor_args),
+        loader.loadTestsFromTestCase(invalid_method_args.invalid_method_args),
+        loader.loadTestsFromTestCase(test_messages.test_messages)
+    ))
+    
+    runner = unittest.TextTestRunner(verbosity = 2)
+    runner.run(suite)
